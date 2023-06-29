65d10ed39b29ce82298309ae93eee2799f274943
Abhishek Anand <anandabhishek3607@gmail.com>
2023-06-29 15:45:19 +0530

    working with patch and delete routes

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 const express = require("express");
-
 const app = express();
-
 const path = require('path');
+const { v4: uuid } = require('uuid');
+const methodOverride = require("method-override");
 
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-const comments = [
+let comments = [
     {
+        id: uuid(),
         username: "Todd",
         comment: 'lol that is so funny!'
     },
     {
+        id: uuid(),
         username: "Tyler",
         comment: 'I would like to go birdwatching with my dog!'
     },
     {
+        id: uuid(),
         username: "Razor",
         comment: 'plz delete your account!'
     },
     {
+        id: uuid(),
         username: 'doga',
         comment: 'woof woof woof'
     }
@@ -32,6 +37,45 @@ const comments = [
 app.get('/comments', (req, res) => {
     res.render('comments/index', { comments });
 })
+
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment, id: uuid() })
+    res.redirect('/comments');
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment })
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment })
+
+})
+
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText
+    res.redirect('/comment')
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
+})
+
 
 
 
